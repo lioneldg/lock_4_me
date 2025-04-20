9d2e09041c8373a809a7cce3abd165dea190d46b
lioneldg <lioneldegans@gmail.com>
2025-04-20 08:51:40 +0200

    refactor(lib): update listen_bluetooth function to handle optional parameters
    - Modified the `listen_bluetooth` function to accept optional `target_uuid` and `rssi_delta_max` parameters.
    - Adjusted the logic to parse the UUID only if provided and updated the RSSI comparison accordingly.
    - Removed unnecessary print statements to streamline output during execution.

--- src-tauri/src/listen_bluetooth.rs
+++ src-tauri/src/listen_bluetooth.rs
@@ -5,14 +5,17 @@ use tauri::Emitter;
 use tokio::time::{timeout, Duration};
 use uuid::Uuid;
 
-#[tauri::command]
+#[tauri::command(rename_all = "snake_case")]
 pub async fn listen_bluetooth(
     app_handle: tauri::AppHandle,
-    target_uuid: String,
-    rssi_delta_max: i16,
+    target_uuid: Option<String>,
+    rssi_delta_max: Option<i16>,
 ) -> Result<(), String> {
-    let target_uuid = Uuid::parse_str(&target_uuid).map_err(|e| e.to_string())?;
-    let mut device_stream = discover_bluetooth_devices(Some(target_uuid))
+    let target_uuid = match target_uuid {
+        Some(uuid_str) => Some(Uuid::parse_str(&uuid_str).map_err(|e| e.to_string())?),
+        None => None,
+    };
+    let mut device_stream = discover_bluetooth_devices(target_uuid)
         .await
         .map_err(|e| e.to_string())?;
     let mut initial_rssi = 0;
@@ -36,16 +39,12 @@ pub async fn listen_bluetooth(
         }
 
         let diff_rssi = rssi - initial_rssi;
-        if rssi_delta_max + diff_rssi > 0 {
+        if rssi_delta_max.map_or(true, |delta_max| delta_max + diff_rssi > 0) {
             let event_type = device.event_type;
             let local_name = device.local_name.unwrap_or_else(|| "Unknown".to_string());
             let id = device.id;
             let rssi = device.rssi.unwrap_or(0);
 
-            println!(
-                "{}: {:?} (id: {}, RSSI: {:?} dBm, diff: {:?} dBm)",
-                event_type, local_name, id, rssi, diff_rssi
-            );
             app_handle
                 .emit(
                     "bluetooth-event",
@@ -59,10 +58,9 @@ pub async fn listen_bluetooth(
                 )
                 .unwrap();
         } else {
+            println!("Connection closed !!!!!!");
             break;
         }
     }
     Ok(())
 }
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
