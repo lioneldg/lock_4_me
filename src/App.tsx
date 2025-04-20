d1fb06eb1a7f256dd82bb7bbe9fd7f02fd7413f9
lioneldg <lioneldegans@gmail.com>
2025-04-20 08:56:39 +0200

    feat(app): refactor App component to enhance Bluetooth functionality and settings management
    - Removed unused state and greet function to simplify the component.
    - Integrated Bluetooth event handling using the new Zustand store.
    - Added functionality to load and write application settings from/to a JSON file.
    - Improved the rendering of Bluetooth events in the UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,81 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
+import { appDataDir, join } from "@tauri-apps/api/path";
 import "./App.css";
+import { Settings, DiscoveredDevice } from "./types";
+import { useBluetoothStore } from "./store/bluetoothStore";
+const TARGET_UUID = "a87e3669-e2de-d0e3-52ce-93a023ceef37";
+const RSSI_DELTA_MAX = 15;
+
+async function listen_bluetooth(targetUuid: string, rssiDeltaMax: number) {
+  await invoke("listen_bluetooth", {
+    target_uuid: targetUuid,
+    rssi_delta_max: rssiDeltaMax,
+  });
+}
+
+async function writeSettings(filePath: string, settings: Settings) {
+  await invoke("write_settings", {
+    file_path: filePath,
+    settings: settings,
+  });
+}
+
+async function loadSettings(filePath: string) {
+  return await invoke("read_settings", { file_path: filePath });
+}
+
+//@ts-ignore
+async function lockScreen() {
+  await invoke("lock_screen");
+}
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-  const [bluetoothEvents, setBluetoothEvents] = useState<any[]>([]);
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const { addEvent, events } = useBluetoothStore();
 
   useEffect(() => {
+    listen_bluetooth(TARGET_UUID, RSSI_DELTA_MAX);
     const unlistenPromise = listen("bluetooth-event", (event) => {
-      setBluetoothEvents((prev) => [...prev, event.payload]);
+      addEvent(event.payload as DiscoveredDevice);
     });
     return () => {
-      unlistenPromise.then((unlisten) => unlisten());
+      unlistenPromise.then((unlisten) => {
+        unlisten();
+      });
     };
+  }, []);
+
+  useEffect(() => {
+    appDataDir().then(async (dir) => {
+      let filePath = await join(dir, "settings.json");
+      let settings;
+      loadSettings(filePath)
+        .then((_settings) => {
+          settings = _settings;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      if (!settings) {
+        writeSettings(filePath, {
+          target_uuid: TARGET_UUID,
+          rssi_delta_max: RSSI_DELTA_MAX,
+        }).catch((error) => {
+          console.error(error);
+        });
+      }
+    });
   }, []);
 
   return (
     <main className="container">
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-
       <h2>Bluetooth Events</h2>
       <ul>
-        {bluetoothEvents.map((e, i) => (
-          <li key={i}>
-            {e.local_name} (RSSI: {e.rssi}, Δ: {e.diff_rssi})
-          </li>
+        {Array.from(events.values()).map((event) => (
+          <p key={event.id}>
+            {event.local_name} {"=>"} RSSI: {event.rssi}, Δ: {event.diff_rssi}
+          </p>
         ))}
       </ul>
     </main>
