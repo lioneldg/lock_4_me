1993dae0062723935aea004aed2ca390c55b5c63
lioneldg <lioneldegans@gmail.com>
2025-04-23 16:19:57 +0200

    feat(slider): add Slider component with styles and functionality
    - Introduced a new `Slider` component for selecting numeric values.
    - Implemented TypeScript interfaces for props, including label, value, min, max, step, and onChange.
    - Added CSS styles for the slider container, label, value display, and slider input.
    - Created an index file for easier exports of the Slider component.

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./style.module.css";
+interface SliderProps {
+  label: string;
+  value: number;
+  min?: number;
+  max?: number;
+  step?: number;
+  onChange: (value: number) => void;
+  id?: string;
+  style?: React.CSSProperties;
+  unit?: string;
+}
+
+const Slider: React.FC<SliderProps> = ({
+  label,
+  value,
+  min = 1,
+  max = 50,
+  step = 1,
+  onChange,
+  id,
+  style,
+  unit,
+}) => {
+  return (
+    <div className={styles.sliderContainer} style={style}>
+      <label htmlFor={id} className={styles.label}>
+        {label}:{" "}
+        <span className={styles.value}>
+          {value} {unit}
+        </span>
+      </label>
+      <input
+        id={id}
+        type="range"
+        min={min}
+        max={max}
+        step={step}
+        value={value}
+        onChange={(e) => onChange(Number(e.target.value))}
+        className={styles.slider}
+      />
+    </div>
+  );
+};
+
+export default Slider;
