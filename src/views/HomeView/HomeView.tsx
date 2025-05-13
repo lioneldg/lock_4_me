a437d609c4ecbad12189f46df67a9c499b93e065
lioneldg <lioneldegans@gmail.com>
2025-05-13 14:31:38 +0200

    feat(homeView): enhance HomeView with Bluetooth device management and new UI elements
    - Added discovery and target mode messages to the translation files for better user guidance.
    - Refactored HomeView to include a more structured layout with a header, title, and device list.
    - Implemented device selection functionality to switch between discovery and target modes.
    - Introduced new CSS styles for improved visual presentation of the HomeView component.

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
+import styles from './style.module.css';
+import React, { useEffect, useMemo } from 'react';
 import { invoke } from '@tauri-apps/api/core';
 import { listen } from '@tauri-apps/api/event';
 import { DiscoveredDevice } from '../../types';
 import { useBluetoothStore } from '../../store/bluetoothStore';
 import { useTranslation } from 'react-i18next';
 import { useSettingsStore } from '../../store/settingsStore';
+import Icon from '../../components/Icon';
+import { ICON_TYPE } from '../../components/Icon/Icon';
+import { useNavigate } from 'react-router';
+import FormattedText from '../../components/FormattedText';
+import { useAppStore } from '../../store/appStore';
 
-async function listen_bluetooth(targetUuid: string, rssiDeltaMax: number) {
+async function listen_bluetooth(targetUuid?: string, rssiDeltaMax?: number) {
   await invoke('listen_bluetooth', {
     target_uuid: targetUuid,
     rssi_delta_max: rssiDeltaMax
@@ -18,12 +24,23 @@ async function lockScreen() {
 }
 
 const HomeView: React.FC = () => {
-  const { addEvent, events } = useBluetoothStore();
+  const { addEvent, events, clearEvents } = useBluetoothStore();
   const { t } = useTranslation();
-  const { settings } = useSettingsStore();
+  const { settings, setSettings } = useSettingsStore();
+  const { isDiscoveryMode, setIsDiscoveryMode } = useAppStore();
+  const navigate = useNavigate();
+
+  const { target_uuid, rssi_delta_max } = useMemo(
+    () => ({
+      target_uuid: settings.target_uuid || undefined,
+      rssi_delta_max: settings.target_uuid ? settings.rssi_delta_max : undefined
+    }),
+    [settings.target_uuid, settings.rssi_delta_max]
+  );
 
   useEffect(() => {
-    listen_bluetooth(settings.target_uuid, settings.rssi_delta_max);
+    listen_bluetooth(target_uuid, rssi_delta_max);
+
     const unlistenBTEventPromise = listen('bluetooth-event', (event) =>
       addEvent(event.payload as DiscoveredDevice)
     );
@@ -34,21 +51,75 @@ const HomeView: React.FC = () => {
       unlistenBTEventPromise.then((unlisten) => unlisten());
       unlistenBTClosedPromise.then((unlisten) => unlisten());
     };
-  }, [settings.target_uuid, settings.rssi_delta_max, addEvent]);
+  }, [target_uuid, rssi_delta_max, addEvent]);
 
-  return (
-    <main>
-      <h1>{t('home.title')}</h1>
-      <p>{t('home.description')}</p>
-      <h2>Bluetooth Events</h2>
+  function selectDevice(device: DiscoveredDevice) {
+    if (isDiscoveryMode) {
+      clearEvents();
+      setSettings({ target_uuid: device.id });
+      setIsDiscoveryMode(false);
+      navigate('/settings');
+    }
+  }
+
+  const settingsButton = (
+    <div
+      className={styles.settings_icon}
+      onClick={() => {
+        navigate('/settings');
+      }}
+    >
+      <Icon type={ICON_TYPE.SETTINGS} />
+    </div>
+  );
+
+  const title = (
+    <div className={styles.title}>
+      <FormattedText style={{ fontSize: 24, fontWeight: 'bold' }}>{t('home.title')}</FormattedText>
+    </div>
+  );
+
+  const modeInfo = (
+    <div className={styles.more_info}>
+      <FormattedText>
+        {isDiscoveryMode
+          ? t('home.discoveryMode')
+          : t('home.targetMode', { uuid: settings.target_uuid })}
+      </FormattedText>
+    </div>
+  );
+
+  const deviceList = (
+    <div className={styles.device_list}>
       <ul>
-        {Array.from(events.values()).map((event) => (
-          <p key={event.id}>
-            {event.local_name} {'=>'} RSSI: {event.rssi} dBm, Δ: {event.diff_rssi} dBm
-          </p>
+        {Array.from(events.values()).map((discoveredDevice) => (
+          <div
+            key={discoveredDevice.id}
+            onClick={() => selectDevice(discoveredDevice)}
+            className="button"
+            style={isDiscoveryMode ? { cursor: 'pointer' } : {}}
+          >
+            <FormattedText>
+              {discoveredDevice.local_name} {'=>'} RSSI: {discoveredDevice.rssi} dBm
+              {!isDiscoveryMode && `, Δ: ${discoveredDevice.diff_rssi} dBm`}
+            </FormattedText>
+          </div>
         ))}
       </ul>
-    </main>
+    </div>
+  );
+
+  return (
+    <>
+      <header className={styles.header}>
+        {title}
+        {settingsButton}
+      </header>
+      <main>
+        {modeInfo}
+        {deviceList}
+      </main>
+    </>
   );
 };
 
