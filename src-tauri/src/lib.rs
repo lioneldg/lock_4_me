016715e9b521abbcbdf4ec550232f1ee20928299
lioneldg <lioneldegans@gmail.com>
2025-05-14 14:38:22 +0200

    feat(logs): show developper tauri logs in console

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 use listen_bluetooth::BluetoothListenerHandle;
 use std::sync::Mutex;
+use tauri_plugin_log;
 mod listen_bluetooth;
 mod lock_screen;
 mod read_write_settings;
@@ -7,6 +8,19 @@ mod read_write_settings;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .clear_targets()
+                .level(log::LevelFilter::Info)
+                .filter(|metadata| {
+                    // Ne garder que les logs de notre application
+                    metadata.target().starts_with(env!("CARGO_CRATE_NAME"))
+                })
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::Stdout,
+                ))
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .manage(BluetoothListenerHandle(Mutex::new(None)))
         .invoke_handler(tauri::generate_handler![
