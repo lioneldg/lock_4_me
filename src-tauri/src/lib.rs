caab2d42c7f6ce072c174d7452cc562c3046ab6c
lioneldg <lioneldegans@gmail.com>
2025-05-15 10:46:18 +0200

    feat(Tauri): implement tray icon functionality
    - Added 'byteorder-lite' and 'image' packages to Cargo.lock for enhanced image handling.
    - Updated Tauri dependency in Cargo.toml to include 'tray-icon' and 'image-png' features.
    - Implemented tray icon functionality in lib.rs, allowing the main window to be hidden or shown via tray icon clicks.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,9 @@
 use listen_bluetooth::BluetoothListenerHandle;
 use std::sync::Mutex;
+use tauri::{
+    tray::{MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    Builder, Manager, WindowEvent,
+};
 use tauri_plugin_log;
 mod listen_bluetooth;
 mod lock_screen;
@@ -7,13 +11,13 @@ mod read_write_settings;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    Builder::default()
         .plugin(
             tauri_plugin_log::Builder::new()
                 .clear_targets()
                 .level(log::LevelFilter::Info)
                 .filter(|metadata| {
-                    // Ne garder que les logs de notre application
+                    // Keep only logs of our application
                     metadata.target().starts_with(env!("CARGO_CRATE_NAME"))
                 })
                 .target(tauri_plugin_log::Target::new(
@@ -23,6 +27,38 @@ pub fn run() {
         )
         .plugin(tauri_plugin_opener::init())
         .manage(BluetoothListenerHandle(Mutex::new(None)))
+        .setup(|app| {
+            let main_window = app.get_webview_window("main").unwrap();
+            let window_ref = main_window.clone();
+
+            main_window.on_window_event(move |event| {
+                if let WindowEvent::CloseRequested { api, .. } = event {
+                    api.prevent_close();
+                    window_ref.hide().unwrap();
+                }
+            });
+
+            TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .icon_as_template(true)
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click { button_state, .. } = event {
+                        if button_state == MouseButtonState::Up {
+                            let app_handle = tray.app_handle();
+                            let window = app_handle.get_webview_window("main").unwrap();
+                            if window.is_visible().unwrap() {
+                                window.hide().unwrap();
+                            } else {
+                                window.show().unwrap();
+                                window.set_focus().unwrap();
+                            }
+                        }
+                    }
+                })
+                .build(app)
+                .expect("failed to create tray icon");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             listen_bluetooth::listen_bluetooth,
             read_write_settings::read_settings,
